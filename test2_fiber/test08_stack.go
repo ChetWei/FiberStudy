d0ab8fe9a270a580f5b3a3efd16cbf9bf8fbec45
wei <chetwei@163.com>
2022-05-18 13:16:18 +0800

    获取路由器的堆栈信息

---
+++ test2_fiber/test08_stack.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+)
+
+func handler(c *fiber.Ctx) error {
+	return c.JSON("{'hello':'world!'}")
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/john/:age", handler)
+	app.Post("/register", handler)
+
+	//获取路由器的堆栈信息
+	data, _ := json.MarshalIndent(app.Stack(), "", "  ")
+
+	app.Get("/info", func(ctx *fiber.Ctx) error {
+		return ctx.SendString(string(data))
+	})
+
+	app.Listen(":3000")
+}
